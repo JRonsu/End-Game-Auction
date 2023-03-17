913cbd070518520debebe924c58da9f2296a778b
Ronsou <samelsonjohn@protonmail.com>
2023-03-16 23:21:08 -0400

    individual account routed

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const databaseManager = require('../models/database-manager');
+const connectToDatabase = require('../config/database');
+const { ObjectId } = require('mongodb');
+
+const account = {
+    Server: 'Server Name',
+    Level: 50,
+    'Gear Score': 500,
+    Ap: 200,
+    Dp: 300,
+    Silver: 1000,
+    Pearls: 500,
+    Skins: ['Skin 1', 'Skin 2', 'Skin 3']
+  };
+  
+
 
 // Render the home page
 router.get('/', async (req, res) => {
@@ -32,7 +47,43 @@ router.get('/classView', async (req, res) => {
   }
 });
 
-module.exports = router;
+router.get('/classView/:className', async (req, res) => {
+    try {
+      const className = req.params.className;
+      const db = await connectToDatabase();
+      const accountsCollection = db.collection('accounts');
+      const accounts = await accountsCollection.find({ Class: className }).toArray();
+      res.render('classDetails', { className, accounts });
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Internal server error');
+    }
+});
+
+router.get('/classView/:className/:accountId', async (req, res) => {
+    try {
+      const className = req.params.className;
+      const accountId = req.params.accountId;
+  
+      // Create an ObjectId instance using the accountId parameter
+      const objectId = new ObjectId(accountId);
+  
+      const db = await connectToDatabase();
+      const accountsCollection = db.collection('accounts');
+      const account = await accountsCollection.findOne({ _id: objectId });
+  
+      res.render('accountDetails', { className, account });
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Internal server error');
+    }
+  });
+  
 
 
+  
+
+  
+
 module.exports = router;
+
